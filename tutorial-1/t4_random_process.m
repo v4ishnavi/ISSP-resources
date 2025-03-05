77ea3679c9c8184103f06aa782ed3ab817872e6c
v4ishnavi <vaishnavishivkumar0@gmail.com>
2025-03-05 10:41:00 +0530

    random proc up

--- tutorial-1/t4_random_process.m
+++ tutorial-1/t4_random_process.m
@@ -1,13 +1,14 @@
 clc; clear; close all;
 
-n_realizations = 5; %(X1(w1,t), X2(w2,t), ...)
-n_samples = 100;    % Number of time samples
-t = 0:n_samples-1;  % Time vector
+% Parameters
+n_realizations = 5;  % Number of realizations
+n_samples = 100;     % Number of time samples
+t = 0:n_samples-1;   % Time vector
 
-% generating the random process....
+% Generate random process (e.g., Gaussian noise)
 X = randn(n_realizations, n_samples);
 
-
+% Plot multiple realizations
 figure;
 hold on;
 for i = 1:n_realizations
@@ -32,9 +33,42 @@ title('Mean of the Random Process');
 grid on;
 
 subplot(2, 1, 2);
-imagesc(cov_X); 
+imagesc(cov_X);
 colorbar;
 xlabel('Time');
 ylabel('Time');
 title('Covariance Matrix of the Random Process');
 grid on;
+
+
+n_realizations_plot = 2; 
+
+maxLag = 20;
+
+for i = 1:n_realizations_plot
+    
+    [acf, lags] = xcorr(X(i, :), maxLag, 'biased'); % acf -> autocorrelation function, lags -> lags
+    
+    mean_adjusted = X(i, :) - mean(X(i, :));
+    [acovf, lags_cov] = xcorr(mean_adjusted, maxLag, 'biased');
+    
+    % Normalize autocorrelation
+    acf = acf / acf(maxLag + 1); 
+    
+    % Plot autocorrelation
+    figure;
+    subplot(2, 1, 1);
+    stem(lags, acf, 'filled');
+    xlabel('Lag');
+    ylabel('Autocorrelation');
+    title(['Autocorrelation of Realization ' num2str(i)]);
+    grid on;
+    
+    % Plot autocovariance
+    subplot(2, 1, 2);
+    stem(lags_cov, acovf, 'filled');
+    xlabel('Lag');
+    ylabel('Autocovariance');
+    title(['Autocovariance of Realization ' num2str(i)]);
+    grid on;
+end
